3173b137b438cbdc4e8f664f2884dca5acdb0baf
jimmarq <jim.marquardson@gmail.com>
2022-05-06 13:42:28 -0400

    Upated powershell piping & about page

--- compile_full_book.ps1
+++ compile_full_book.ps1
@@ -1,1 +1,6 @@
+# These variables seem to be ignored. Process
+#Set-Variable -Name "PUPPETEER_DEFAULT_TIMEOUT" -Value "60000"
+#Set-Variable -Name "PUPPETEER_NAVIGATION_TIMEOUT" -Value "60000"
+#Set-Variable -Name "PUPPETEER_RENDERING_TIMEOUT" -Value "60000"
+
 asciidoctor-web-pdf Cybersecurity_Fundamentals.adoc -a stylesheet="+Cybersecurity_Fundamentals.css"
